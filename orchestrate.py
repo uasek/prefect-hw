fb15843b87bfbdca3bacee075aeeb2d2eb7131f5
Mikhail Baranov <uasekus@gmail.com>
2023-06-11 19:15:57 +0400

    add markdown report

--- orchestrate.py
+++ orchestrate.py
@@ -9,6 +9,8 @@ from sklearn.metrics import mean_squared_error
 import mlflow
 import xgboost as xgb
 from prefect import flow, task
+from prefect.artifacts import create_markdown_artifact
+from datetime import datetime
 
 
 @task(retries=3, retry_delay_seconds=2, name="Read taxi data")
@@ -108,6 +110,19 @@ def train_best_model(
         mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
 
         mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+
+        report = f"""# Model results
+
+        |   Created time   | RMSE on val |
+        |        ---       |     ---     |
+        | {datetime.now()} |  {rmse:.3f} |
+        """
+
+        create_markdown_artifact(
+            markdown=report,
+            key="example_report"
+        )
+
     return None
 
 
